e697b1efd6bfc73ae5c8c21bf6ef325f0629121d
= <patrickjosephfleming@gmail.com>
2021-02-01 17:40:51 +0000

    initial setup

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/patrickjfl/gqlgen-todos
+
+go 1.15
+
+require (
+	github.com/99designs/gqlgen v0.13.0
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/golang-migrate/migrate/v4 v4.14.1 // indirect
+	github.com/vektah/gqlparser/v2 v2.1.0
+)
---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+
+	database.InitDB()
+	database.Migrate()
+	server := handler.GraphQL(hackernews.NewExecutableSchema(hackernews.Config{Resolvers: &hackernews.Resolver{}}))
+	router.Handle("/", handler.Playground("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
